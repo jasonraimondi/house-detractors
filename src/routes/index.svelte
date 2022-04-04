5ec2e240ce39beb8bb28e0eed081d62b93bde99f
Jason Raimondi <jason@raimondi.us>
2022-04-03 23:43:39 -0700

    refactor: prettier

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,10 +7,14 @@
   import { detractorsMap } from "../lib/map";
 
   const sum = (a: number, b: number) => a + b;
-  const toPercent = (a: number): number => 100 - (a * 100);
+  const toPercent = (a: number): number => 100 - a * 100;
 
-  const controlHigh = Object.values(detractorsMap).map(m => 10 * m.high).reduce(sum);
-  const controlLow = Object.values(detractorsMap).map(m => 10 * m.low).reduce(sum);
+  const controlHigh = Object.values(detractorsMap)
+    .map((m) => 10 * m.high)
+    .reduce(sum);
+  const controlLow = Object.values(detractorsMap)
+    .map((m) => 10 * m.low)
+    .reduce(sum);
 
   let newHouse = {
     name: "New House",
@@ -40,11 +44,11 @@
       name: "The Big Pool",
       detractors: {
         no_current_pool: 0,
-        no_room_for_grass: 5,
+        no_room_for_grass: 7,
         needs_floor_replaced: 10,
         needs_kitchen_replaced: 3,
         needs_master_bathroom_replaced: 8,
-        no_gas_stove: 7,
+        no_gas_stove: 9,
         closed_floor_plan: 3,
         small_kitchen: 1,
         small_master_bath: 10,
@@ -58,32 +62,10 @@
         old_roof: 0,
       },
     },
-    {
-      "name": "New Potential House",
-      "detractors": {
-        "no_current_pool": 10,
-        "no_room_for_grass": 10,
-        "needs_floor_replaced": 0,
-        "needs_kitchen_replaced": 0,
-        "needs_master_bathroom_replaced": 0,
-        "no_gas_stove": 0,
-        "closed_floor_plan": 0,
-        "small_kitchen": 0,
-        "small_master_bath": 0,
-        "no_garage": 0,
-        "small_garage": 0,
-        "low_storage": 0,
-        "needs_drywalls_replaced": 0,
-        "has_textured_walls": 0,
-        "small_yard": 0,
-        "old_house": 0,
-        "old_roof": 0,
-      },
-    },
   ];
 
   $: {
-    houses = houses.map(h => {
+    houses = houses.map((h) => {
       let lowTotal = 0;
       let highTotal = 0;
 
@@ -92,8 +74,8 @@
         highTotal += h.detractors[detractorName] * detractorsMap[detractorName].high;
       }
 
-      const low = (lowTotal / controlHigh);
-      const high = (highTotal / controlHigh);
+      const low = lowTotal / controlHigh;
+      const high = highTotal / controlHigh;
       console.log(low, high);
 
       h.calculated = {
@@ -111,31 +93,37 @@
 </script>
 
 <ul>
-    {#each Object.values(detractorsMap) as detractor}
-        <li>{detractor.description}: {detractor.low}, {detractor.high}</li>
-    {/each}
+  {#each Object.values(detractorsMap) as detractor}
+    <li>{detractor.description}: {detractor.low}, {detractor.high}</li>
+  {/each}
 </ul>
 
 {#each houses as home}
-    <div class="home">
-        <p><strong class="home-title">{home.name}:</strong></p>
-        <p><span class="home-percent">Match: <Percent percent={home.calculated.percent}/></span></p>
+  <div class="home">
+    <p><strong class="home-title">{home.name}:</strong></p>
+    <p><span class="home-percent">Match: <Percent percent={home.calculated.percent} /></span></p>
 
-        {#each Object.keys(home.detractors) as detractorName}
-            <label>
-                <span>{detractorsMap[detractorName].description}:</span>
-                <input type="number" max="10" min="0" bind:value={home.detractors[detractorName]}>
-            </label>
-        {/each}
-    </div>
+    {#each Object.keys(home.detractors) as detractorName}
+      <label>
+        <span>{detractorsMap[detractorName].description}:</span>
+        <input type="number" max="10" min="0" bind:value={home.detractors[detractorName]} />
+      </label>
+    {/each}
+  </div>
 {/each}
 
 <div>
-    <button on:click={addHome}>Add Home</button>
+  <button on:click={addHome}>Add Home</button>
 </div>
 
 <div>
-    <pre><code>{JSON.stringify(houses.map(({calculated, ...h}) => h), null, 2)}</code></pre>
+  <pre><code
+      >{JSON.stringify(
+        houses.map(({ calculated, ...h }) => h),
+        null,
+        2,
+      )}</code
+    ></pre>
 </div>
 
 <style>
